9b9725c6dd899e3522233f4459b66e27a89539fe
yalkhateebra-gif <yalkhateeb.r.a@gmail.com>
2026-05-16 17:21:12 +0300

    create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+import time
+name=input("Enter your name: ")
+time.sleep(1)
+print("Hello " + name + "!")
+time.sleep(1)
+ee=input("1+1=")
+time.sleep(1)
+if ee=="2":
+    print("Correct!")
+else:
+    print("Incorrect!")
+    quit()
+oo=input("33*32=")
+time.sleep(1)
+if oo=="1056":
+    print("Correct!")
+else:
+    print("Incorrect!")
+    quit()
+pp=input("500/8=")
+time.sleep(1)
+if pp=="62.5":
+    print("Correct!")
+else:
+    print("Incorrect!")
+    quit()
+qq=input("244**2=")
+time.sleep(1)
+if qq=="59536":
+    print("Correct!")
+else:
+    print("Incorrect!")
+    quit()
+ee=input("1-100+1-66*5/2=")
+time.sleep(1)
+if ee=="-263":
+    print("Correct!")
+else:
+    print("Incorrect!")
+    quit()
+o=input("Do you want to solve the most difficult question? (yes/no): ")
+time.sleep(1)
+if o=="yes":
+    print("Okay, let's solve it!")
+else:
+    print("Okay, maybe next time!")
+    quit()
+we=input("4-4-4-4+55*44*2=")
+time.sleep(2)
+if we=="4832":
+    print("Correct!")
+else:
+    print("Incorrect!")
+    quit()
+print("the winner is "+name+"!")
+time.sleep(2)
+print("Thank you for playing "+name+"!")
